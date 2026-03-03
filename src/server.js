4814542de77277c78497172de99f5082abf4d04b
Sumit Kumar <sumitku2938k@gmail.com>
2026-03-03 20:11:40 +0530

    PHASE 0 – Project Setup & Environment  ✅

--- src/server.js
+++ src/server.js
@@ -1,11 +1,21 @@
+require('dotenv').config(); // Load environment variables from .env file
+
 const express = require("express");
 const app = express();
-const router = require("./router/auth-router.js");
-const PORT = 3000;
+const router = require("./routes/auth-router.js");
+const PORT = process.env.PORT || 3000;
+const connectDB = require('./config/db');
 
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use("/api", router); // Use the router for API routes
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-})
+// Start the server
+connectDB().then(() => { // Ensure DB is connected before starting server
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}).catch((err) => {
+    console.error('Failed to start server:', err);  
+});
+
+    
